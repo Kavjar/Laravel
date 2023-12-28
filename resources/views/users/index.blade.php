3f0f751aed9729997bf78074efbedf2a6ab158ed
Yaroslav Karabin <55619450+Kavjar@users.noreply.github.com>
2023-12-28 15:34:04 +0200

    destroy metod
    users destroy route

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,15 +4,17 @@
 
 @section('content')
     <h2>Users</h2>
-    <table border="1">
+        <table border="1">
         <tr>
             <th>Name</th>
             <th>Email</th>
+            <th>Details</th>
         </tr>
         @foreach($users as $user)
             <tr>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
+                <td><a href="{{ route('users.show', $user) }}">Details</a></td>
             </tr>
         @endforeach
     </table>
